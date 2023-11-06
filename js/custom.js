87f9db286446d45916dbc56979f4a3a7486a4f2a
SpecterLLL <3196480050@qq.com>
2023-11-06 21:23:51 +0800

    Site updated: 2023-11-06 21:23:50

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-var navbarWidget = document.getElementById("navbar");
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>var navbarWidget = document.getElementById("navbar");
 
 function toggleNavbar() {
   var prevScrollPos = window.pageYOffset || document.documentElement.scrollTop;
